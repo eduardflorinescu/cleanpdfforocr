7f258c0cc968843243c86627d9e5da504547377a
eduardflorinescu <georgeflorinescu@gmail.com>
2019-10-12 13:17:54 +0300

    Add lowe size options

--- get_pdf.sh
+++ get_pdf.sh
@@ -8,10 +8,16 @@ ls ./pg*.pdf | xargs -L1 -I {}  inkscape {} -z --export-dpi=300 --export-area-dr
 rm *.pdf
 #ls ./p*.png | xargs -L1 -I {} convert {}  -quality 100 -density 300 -fill white -fuzz 80% +opaque "#000000" -density 300  {}.jpg
 ls ./p*.png | xargs -L1 -I {} convert {}  -quality 75 -density 300 -fill white -fuzz 20% +opaque "#000000" -density 300  {}.jpg
-#ls ./p*.png | xargs -L1 -I {} convert {}  -set colorspace Gray  -quality 55 -density 300 -fill white -fuzz 40% +opaque "#000000" -density 300 {}.jpg
+#ls ./p*.png | xargs -L1 -I {} convert {} -strip -interlace JPEG -sampling-factor 4:2:0  -colorspace Gray -gaussian-blur 0.05 -quality 55 -density 300 -fill white -fuzz 40% +opaque "#000000" -density 300 {}.jpg
+#ls ./p*.png | xargs -L1 -I {} convert {} -strip -interlace JPEG -sampling-factor 4:2:0  -colorspace Gray -gaussian-blur 0.05 -quality 20 -density 300 -fill white -fuzz 40% +opaque "#000000" -density 300 {}.jpg
+
+#ls ./p*.png | xargs -L1 -I {} convert {} -strip -interlace JPEG -sampling-factor 4:2:0 -adaptive-resize 60% -set colorspace Gray  -quality 55 -density 300 -fill white -fuzz 40% +opaque "#000000" -density 300 {}.jpg
+
 ls ./p*.png | xargs -L1 -I {} convert {}  -set colorspace Gray  -density 300 -fill white -fuzz 40% +opaque "#000000" -density 300 {}.clean.png
 
 rm *.pdf
 ls -1 ./*jpg | xargs -L1 -I {} img2pdf {} -o {}.pdf
 rm *.jpg
 pdftk *.pdf cat output combined.pdf
+
+
